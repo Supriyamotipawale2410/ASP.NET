1eefac7cc98bc491342df6090d7e82a45111e93c
supriya <supriyamotipawale2019@gmail.com>
2024-12-13 23:42:38 +0530

    first commit

---
+++ WebApplication4/Controllers/SIController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class SIController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(SI obj)
+        {
+            ViewBag.result= "Simple Interest : " + (obj.P * obj.T * obj.T)/100;
+            return View();
+        }
+    }
+}
---
+++ WebApplication4/Models/SI.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication4.Models
+{
+    public class SI
+    {
+        private float p;
+        private float r;
+        private float t;
+
+        public float P { get => p; set => p = value; }
+        public float R { get => r; set => r = value; }
+        public float T { get => t; set => t = value; }
+    }
+}
